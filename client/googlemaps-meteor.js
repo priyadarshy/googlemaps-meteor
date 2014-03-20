ae8f617679d65a5386843274dd1f0531f7d839c0
ashutosh <ashutosh@eemelabs.com>
2014-03-19 22:46:34 -0700

    Allows you to search for locations and display precomputed locations

--- client/googlemaps-meteor.js
+++ client/googlemaps-meteor.js
@@ -1,34 +1,21 @@
+/*
+ * googlemaps-meteor.js
+ */
 
-// Simulate a location we have stored.
-var testStartingData =  {name:"San Francisco",
+var testStartingData = {
+  name:"San Francisco",
   address:"San Francisco, CA, USA",
-  coordinate:{lat:37.7749295,lng:-122.41941550000001}
-}
+  coordinate: {lat:37.7749295,lng:-122.41941550000001}
+};
 
-if (Meteor.isClient) {
+Template.mapShow.created = function() {
+  Session.set("mapInitialLocation", testStartingData);
+};
 
-  Template.mapShow.created = function() {
+Template.mapShow.destroyed = function() {
 
-    Session.set("mapInitialLocation", testStartingData);
-    // Wait until the map is ready then go to location.
-    Deps.autorun(function (c) {
-      if (! Session.get("mapsReady")){
-        console.log("not ready");
-        return;
-      }
+};
 
-      Template.maps.centerOnLocation(Session.get("mapInitialLocation"));
-      c.stop();
-    });
-  };
+Template.mapShow.rendered = function() {
 
-  Template.mapShow.rendered = function() {
-  };
-
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+};
--- client/maps.js
+++ client/maps.js
@@ -5,47 +5,23 @@
  */
 
 currentLocation = null;
+currentMap =  null;
 
 Template.maps.created = function() {
-    // Assign this maps template a property called label.
-    this['_label'] = _.uniqueId('mapsTemplate_');
-};
-
-Template.maps.rendered = function() {
-
-  // If we can't find the map with this template's label
-  // in the global map list. Initialize one and add it.
-  if (!_.findWhere(globalMaps, {_label:this._label})) {
-    globalMaps[this._label] = gmaps.initialize(null, this, "pac-input", Template.maps.locationUpdatedCallback);	
-    Session.set("mapsReady", true);
-  } else {
-    // Otherwise re-initialize an old map.
-    gmaps.initialize(globalMapList[this._label], this, "pac-input");
-  }
-};
-
-Template.maps.locationUpdatedCallback = function(newLocation) {
-    currentLocation = newLocation;
-};
-
-Template.maps.centerOnLocation = function(mapStartingLocation) {
-  console.log(">>>> Centering on location >>>>");
-  // Create the google maps marker.	
-  var locationMarker = gmaps.addMarker(
-    {"lat":mapStartingLocation.coordinate.lat,
-      "lng":mapStartingLocation.coordinate.lng,
-      "title":mapStartingLocation.name});
-      // Add the marker to the map.
-      currentMap = _.values(globalMaps)[0][0]; // Eventually this needs to get the correct map.
-      locationMarker.setMap(currentMap);
-      // Center the map to the marker and zoom appropriately.
-      currentMap.setCenter(new google.maps.LatLng(mapStartingLocation.coordinate.lat, mapStartingLocation.coordinate.lng));
-      currentMap.setZoom(16);
+  Session.set("mapsReady", false);
 };
 
 Template.maps.destroyed = function() {
-  // Remove the map from globalMaps associated with this Template.
-  _.omit(globalMaps, this._label);
+  Session.set("mapsReady", false);
+};
+
+Template.maps.rendered = function() {
+  currentMap = gmaps.initialize("pac-input", Template.maps.locationUpdatedCallback, Session.get("mapInitialLocation"));	
+  Session.set("mapsReady", true);
+};
+
+Template.maps.locationUpdatedCallback = function(newLocation) {
+  currentLocation = newLocation;
 };
 
 // This can probably be deleted at some point.
